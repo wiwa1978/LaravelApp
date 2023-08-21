b2c54963e217a41e1b3504d8ea22b22a887f23fe
Wim Wauters <wauterw@gmail.com>
2023-08-21 20:03:57 +0200

    test

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -7,6 +7,9 @@ echo "Deployment started ..."
 # if already is in maintenance mode
 (php artisan down) || true
 
+# Stash changes
+git stash
+
 # Pull the latest version of the app
 git pull origin main
 
